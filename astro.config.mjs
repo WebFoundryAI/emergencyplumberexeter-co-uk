57a52c5cc2dc8db18c48297f01a516944c92e331
Steve Wyman <cloudventiresonline@gmail.com>
2026-03-14 11:33:16 +0000

    chore: configure production domain emergencyplumberexeter.co.uk

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://emergencyplumberexeter.co.uk/',
+  site: 'https://emergencyplumberexeter.co.uk',
   integrations: [
     sitemap(),
     tailwind(),
